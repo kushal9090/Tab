380b2e5c149c73cdf9a215f7558afa57c1f9e405
kushal mahindrakar <kushmahindrakar@gmail.com>
2017-06-24 12:10:25 +0530

    fragment and tabb

---
+++ app/src/main/java/com/kushal/practicefromstart/CallsFragment.java
@@ -0,0 +1,23 @@
+package com.kushal.practicefromstart;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kusha on 6/7/2017.
+ */
+
+public class CallsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main , container , false);
+
+
+    }
+}
